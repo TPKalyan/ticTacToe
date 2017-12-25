eb1de1a07ad338950c6f82e439895ff9181f0634
Pavan kalyan <pavankalyanthota@gmail.com>
2017-12-25 16:21:44 +0530

    added some test cases for game object

--- test/gameTest.js
+++ test/gameTest.js
@@ -2,11 +2,51 @@ const assert = require('assert');
 const Game = require('../src/game.js')
 let test = {};
 
-test["getCurrentPlayer should return first player Object if no moves are made"] = function() {
-  let newGame = new Game();
-  assert.deepEqual(newGame.getCurrentPlayer(),newGame.players[0]);
+test["getCurrentPlayer should return first player Object if even moves are made"] = function() {
+  let game = new Game();
+  assert.deepEqual(game.getCurrentPlayer(),game.players[0]);
+  game.makeMove(1);
+  game.makeMove(2);
+  assert.deepEqual(game.getCurrentPlayer(),game.players[0]);
+
 }
 
+test["getNextPlayer should return second player Object if odd moves are made"] = function() {
+  let game = new Game();
+  game.makeMove(8);
+  assert.deepEqual(game.getCurrentPlayer(),game.players[1]);
+}
 
+test["doesPositionOccupied should return true if the position is occupied already"] = function() {
+  let game = new Game();
+  game.makeMove(1);
+  assert.ok(game.doesPositionOccupied(1));
+}
+
+test["doesPositionOccupied should return false if the position is'nt occupied already"] = function() {
+  let game = new Game();
+  assert.ok(!game.doesPositionOccupied(1));
+}
+
+test["isDraw should return true if all positions are occupied"] = function(){
+  let game = new Game();
+  game.makeMove(1);
+  game.makeMove(2);
+  game.makeMove(3);
+  game.makeMove(4);
+  game.makeMove(5);
+  game.makeMove(6);
+  game.makeMove(7);
+  game.makeMove(8);
+  game.makeMove(9);
+  assert.ok(game.isDraw());
+}
+
+test["isDraw should return false if all positions aren't occupied"] = function(){
+  let game = new Game();
+  game.makeMove(8);
+  game.makeMove(9);
+  assert.ok(!game.isDraw());
+}
 
 exports.test = test;
